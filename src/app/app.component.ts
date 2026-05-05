150f4e04a8c51fca6d3d4322368e01358cdc0152
LeidyMaingualDev <vanesamaingual2008@gmail.com>
2026-05-05 13:55:31 -0500

    feat(footer): add responsive footer with links and copyright

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './shared/principalShares/nav/nav.component';
+import { FooterComponent } from './shared/principalShares/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
