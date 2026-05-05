76ef07e2c0050f5c081ff804c990c01c505275df
LeidyMaingualDev <vanesamaingual2008@gmail.com>
2026-05-05 15:13:10 -0500

    Merge pull request #2 from LeidyMaingualDev/feature/hero-section
    Feature/hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './shared/principalShares/nav/nav.component';
 import { FooterComponent } from './shared/principalShares/footer/footer.component';
+import { HomeComponent } from './features/landing/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, FooterComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
