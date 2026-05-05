039738e7a998ababba781af6dd016b07653572f6
LeidyMaingualDev <vanesamaingual2008@gmail.com>
2026-05-05 17:09:49 -0500

    fix(home): add missing home component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './shared/principalShares/nav/nav.component';
+<<<<<<< Updated upstream
+=======
+import { FooterComponent } from './shared/principalShares/footer/footer.component';
+import { HomeComponent } from './features/landing/home/home.component';
+import { FeaturesSectionComponent } from './features/landing/features-section/features-section.component';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-root',
   standalone: true,
+<<<<<<< Updated upstream
   imports: [RouterOutlet, NavComponent],
+=======
+  imports: [RouterOutlet, NavComponent, FooterComponent, HomeComponent, FeaturesSectionComponent],
+>>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
