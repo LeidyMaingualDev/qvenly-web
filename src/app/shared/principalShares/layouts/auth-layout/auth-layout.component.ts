3c27acf0fc87f4ce6dd082a9ac0d2cead6b59a4a
LeidyMaingualDev <vanesamaingual2008@gmail.com>
2026-05-16 21:39:41 -0500

    feat(visual): improve landing login register and add reset password page

--- src/app/shared/principalShares/layouts/auth-layout/auth-layout.component.ts
+++ src/app/shared/principalShares/layouts/auth-layout/auth-layout.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-auth-layout',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './auth-layout.component.html',
+  styleUrl: './auth-layout.component.css'
 })
-export class AppComponent {
-  title = 'qvenly-web';
-}
+export class AuthLayoutComponent {}
